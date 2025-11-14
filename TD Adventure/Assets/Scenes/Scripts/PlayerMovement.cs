32270c05ad0686943a67dd832a6755b2cae5675b
dddeandre32-dotcom <dddeandre32@gmail.com>
2025-11-14 09:38:54 -0500

    AddedPlayerMovement

---
+++ TD Adventure/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    //FEATURE LIST
+    // Use WASD keys for movement
+    // need a variable for speed
+    // we need to store the direction of our character
+    // need a variable for running
+    // Special movements example: rolling or dashing
+    // mantling
+    // If we can climb or not
+
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
